385f7aba6a443f0a1af75f8463d273ae0b804598
sriramyy <131324693+sriramyy@users.noreply.github.com>
2024-10-04 12:42:02 -0400

    Update analyzer.cpp

--- analyzer.cpp
+++ analyzer.cpp
@@ -5,6 +5,36 @@ using namespace std;
 #include <vector>
 #include <cstdio>
 
+int binary_search(vector<string> data, string element){
+
+    int low = 0;
+    int high = size(data);
+    int mid = (low + high) / 2;
+    while (low <= high){
+      if (data[mid] == element){
+        return mid;
+      } else if (data[mid] < element){
+        low = mid + 1;
+      } else {
+          return mid;
+      }
+     }
+
+    return -1;
+}
+
+
+int linear_search(vector<string> data, string element){
+    for (int i = 0; i < data.size(); i++){
+      if (data[i] == element){
+        return i;
+        }
+    }
+
+    return -1;
+}
+
+
 
 int search(vector<string> data, string search_item) {
     std::cout << "Search for " << search_item;
@@ -13,12 +43,13 @@ int search(vector<string> data, string search_item) {
     int x = linear_search(data, search_item);
     auto end = chrono::high_resolution_clock::now();
 
-    std::cout << "[not_here]"
+    std::cout << "[not_here] Linear Search: " << (start-end) << " at position " << x;
 
+    start = chrono::high_resolution_clock::now();
+    int y = binary_search(data, search_item);
+    end = chrono::high_resolution_clock::now();
 
-
-
-
+    std::cout << "[not_here] Binary Search: " << (start-end) << " at position " << x;
 
     return 0;
 }
@@ -29,6 +60,9 @@ int main() {
     vector<string> data = {"aaaaa","bbbbb"};
 
     search(data, "not here");
+    search(data, "aaaa");
+    search(data, "zzzz");
+
 
 
 
